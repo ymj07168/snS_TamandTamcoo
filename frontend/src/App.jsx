88847f0ea4fd8e485fee576cf8540effd13a689f
jeongeun <wjddms9921@gmail.com>
2023-01-09 02:07:27 +0900

    [front] ADD : 초기 세팅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/style.js
@@ -0,0 +1,7 @@
+import {css} from "@emotion/react";
+
+export const Color = {
+    yellow : '#FFEAB5',
+    blue : '#7ACEF2',
+    gray : '#e6e6e6'
+}
