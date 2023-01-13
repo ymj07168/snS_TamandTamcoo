bdc4574d2e9db6d0419eea9660662146c1c5f87a
이효원 <ymj07168@gmail.com>
2023-01-13 15:54:20 +0900

    move: routes 폴더로 user 코드 이동

--- TamandTamcoo_Back/index.js
+++ TamandTamcoo_Back/index.js
@@ -1,93 +1,24 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
 const config = require('./config/key');
-const { auth } = require('./middleware/auth');
-const { User } = require("./models/User");
 
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 mongoose.set('strictQuery', false)
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/api/users/register', (req, res) => {
-    // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
-
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-        if (err) return res.json({ success: false, err })
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
-
-app.post('/api/users/login', (req, res) => {
-    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user) {
-            return res.json({
-                loginSuccess: false,
-                message: "제공된 이메일에 해당하는 유저가 없습니다."
-            })
-        }
-
-        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-
-            //비밀번호까지 맞다면 토큰을 생성한다.
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                // 토큰을 저장한다. 쿠키, 로컬스토리지
-                res.cookie("x_auth", user.token)
-                    .status(200)
-                    .json({ loginSuccess: true, userId: user._id })
-            })
-        })
-    })
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 
-app.get('/api/users/auth', auth, (req, res) => {
-    //여기까지 미들웨어를 통과해왔다는 얘기는 authentication이 true라는 것
+app.use('/api/users', require('./routes/users'));
 
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role,
-        image: req.user.image
-    })
-})
-
-app.get('/api/users/logout', auth, (req, res) => {
-    console.log('req.user', req.user)
-    User.findOneAndUpdate({ _id: req.user._id },
-        { token: "" },
-        (err, user) => {
-            if (err) return res.json({ success: false, err });
-            return res.status(200).send({
-                success: true
-            })
-        })
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
