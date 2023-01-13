43e9835c8487b605a7c6acc6c13ca365127b192f
jeongeun <wjddms9921@gmail.com>
2023-01-13 22:36:10 +0900

    [front] ADD : 스티커 드래그 기능 추가

--- frontend/src/pages/decoration/index.jsx
+++ frontend/src/pages/decoration/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Draggable from "react-draggable";
 
 //component
 import Header from "../../components/header/index";
@@ -10,14 +11,26 @@ import Button from "../../components/button/index";
 import { Container } from "./style";
 
 const Index = () => {
-  const [sticker, setSticker] = useState("");
+  const { state } = useLocation();
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const handleStickerDrag = (position) => {
+    setPosition({ x: position.x, y: position.y });
+  };
+
   return (
-    <Container>
+    <Container position={position}>
       <Header text={"탐라 꾸미기"} />
       <div className="container__title">제목</div>
       <div className="img-wrap">
-        <img src={""} className='img-wrap__bg'/>
-        <img src={sticker} className='img-wrap__sticker'/>
+        <img src={""} className="img-wrap__bg" />
+        {state && (
+          <Draggable
+            onDrag={(e, data) => handleStickerDrag(data)}
+            bounds={"parent"}
+          >
+            <img src={state} className="img-wrap__sticker" />
+          </Draggable>
+        )}
       </div>
       <Link to="/sticker-select">
         <Button text={"스티커 선택하기"} style={{ marginBottom: "10px" }} />
--- frontend/src/pages/stickerSelect/index.jsx
+++ frontend/src/pages/stickerSelect/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 //component
 import Header from "../../components/header/index";
@@ -7,6 +8,8 @@ import Header from "../../components/header/index";
 import { Container } from "./style";
 
 const Index = () => {
+  const navigate = useNavigate();
+
   const imgList = [
     "https://png.pngtree.com/png-vector/20190130/ourmid/pngtree-autumn-maple-leaf-element-design-leaf-elementautumn-leavesautumn-png-image_602484.jpg",
     "https://png.pngtree.com/png-vector/20210923/ourmid/pngtree-autumn-leaves-border-hello-october-greeting-png-image_3952783.png",
@@ -19,7 +22,12 @@ const Index = () => {
       <Header text={"스티커 선택"} link={"/decoration"} />
       <div className="img-list">
         {(imgList || []).map((item) => {
-          return <img src={item} />;
+          return (
+            <img
+              src={item}
+              onClick={() => navigate("/decoration", { state: item })}
+            />
+          );
         })}
       </div>
     </Container>
