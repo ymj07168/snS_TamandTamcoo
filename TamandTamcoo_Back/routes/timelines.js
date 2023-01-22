caadfcdc11b82720f5a635d2f6bb938b6b6729a4
이효원 <ymj07168@gmail.com>
2023-01-22 16:18:37 +0900

    feat: 타임라인 생성 api

--- TamandTamcoo_Back/routes/timelines.js
+++ TamandTamcoo_Back/routes/timelines.js
@@ -17,7 +17,7 @@ router.get('/auth', auth, (req, res) => {
 })
 
 // 사용자 이메일 타임라인 목록 조회 
-router.get('/', auth, (req, res) => {
+router.get('/read', auth, (req, res) => {
     Timeline.find({ eamil: req.body.email },
         (err, timelines) => {
             if (err) return res.json({ success: false, err });
@@ -27,8 +27,20 @@ router.get('/', auth, (req, res) => {
         })
 })
 
-router.post('/', auth, (req, res) => {
-    Timeline.
+// 타임라인 생성
+router.post('/create', auth, (req, res) => {
+    const timeline = new Timeline(req.body)
+
+    timeline.save((err, timeline) => {
+        if (err) return res.json({ success: false, err })
+
+        Timeline.find({ '_id': timeline._id })
+            .populate('uid')
+            .exec((err, timelines) => {
+                if (err) return res.json({ success: false, err });
+                return res.status(200).send({ success: true, timelines })
+            })
+    })
 })
 
 module.exports = router;
