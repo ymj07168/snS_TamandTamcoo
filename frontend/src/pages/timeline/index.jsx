c5e73f92224c7faf74866bd3e5735b89360cfde9
이효원 <ymj07168@gmail.com>
2023-02-03 02:40:02 +0900

    test: 탐라 전체 목록 조회 테스트 중

--- frontend/src/pages/timeline/index.jsx
+++ frontend/src/pages/timeline/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 //component
 import Header from "../../components/header/index";
@@ -9,9 +11,29 @@ import Button from "../../components/button/index";
 import { Container } from "./style";
 
 const Index = () => {
+
+  const [timelines, setTimelines] = useState('');
+
+  useEffect(() => {
+    getTimelineAll();
+  }, []);
+
+  const getTimelineAll = async () => {
+    try {
+      const res = await axios.get(`/api/timelines/read`);
+      setTimelines(res.data.timelines);
+      console.log(res.data)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container>
       <Header text={"타임라인"} link={"/"} />
+      <div>
+        {timelines ? timelines.map((timeline, index) => (<img key={index} src={timeline.bid.imgURL} alt="이미지 없음" />)) : <div>Loading...</div>}
+      </div>
       <Link to="/creation">
         <Button text={"타임라인 추가하기"} style={{ marginBottom: "10px" }} />
       </Link>
