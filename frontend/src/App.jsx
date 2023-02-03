4ccff7552cd220197268658603038c4c1487b6f0
이효원 <ymj07168@gmail.com>
2023-02-03 16:56:13 +0900

    feat: 탐라 상세 페이지 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home/index";
 import Timeline from "./pages/timeline/index";
+import Detail from "./pages/detail/index";
 import Creation from "./pages/creation/index";
 import Decoration from "./pages/decoration/index";
 import StickerSelect from "./pages/stickerSelect/index";
@@ -11,11 +12,13 @@ import Complete from "./pages/complete/index";
 import Login from "./pages/login/index";
 import Signup from "./pages/signup/index";
 
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/timeline" element={<Timeline />} />
+      <Route path="/detail" element={<Detail />} />
       <Route path="/creation" element={<Creation />} />
       <Route path="/decoration" element={<Decoration />} />
       <Route path="/sticker-select" element={<StickerSelect />} />
